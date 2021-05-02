3dcc6ccfea8170a8b192b65032b270e8ebca476f
Brandon Vout <git@brandonvout.com>
2021-05-02 03:46:17 -0400

    Added aliases for changing permissions.

--- bash/my_shells.sh
+++ bash/my_shells.sh
@@ -16,11 +16,14 @@ source "toggle_hidden.sh"
 source "add_license.sh"
 source "add_readme.sh"
 source "add_ignore.sh"
+source "read_write.sh"
 
 # source scripts made by others
+OTHER_SHELLS="$HOME/Shells"
 # source $HOME/.oh-my-git/prompt.sh
+source "$OTHER_SHELLS/bash/rst_to_md.sh"
 
 # Goto my custom shell folder(s)
 alias myshells='cd $HOME/Development/Shell'	# local shells made by me
-alias lshells='cd $HOME/Shells'				# local shells (not made by me)
+alias lshells='cd $OTHER_SHELLS'			# local shells (not made by me)
 alias gshells='cd "$HOME/Google Drive/1_Sync_Unenc/MyProjects/Shell"'
