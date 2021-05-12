cd5dabe3e4cf21925868fa9c0346482f22117e1f
Brandon Vout <git@brandonvout.com>
2021-05-12 04:52:05 -0400

    updated readme paths

--- bash/add_readme.sh
+++ bash/add_readme.sh
@@ -7,8 +7,8 @@ DEST_README="./README"
 
 # local readme locations
 LOCAL_README_PATH="$HOME/.readmes"
-README="$LOCAL_README_PATH/README.md"
-README_GAME="$LOCAL_README_PATH/README.game.md"
+README="$LOCAL_README_PATH/readme.generic.md"
+README_GAME="$LOCAL_README_PATH/readme.game.md"
 
 # copy local readme to current directory
 alias cp-readme='cp $README $DEST_README'
