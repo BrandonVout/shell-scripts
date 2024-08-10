5a5c8d044d5043c10b0d5a9b9c9358633ae6d974
Brandon Vout <git@brandonvout.com>
2024-08-10 00:26:46 -0400

    fixed typo
    Signed-off-by: Brandon Vout <git@brandonvout.com>

--- bash/etherpad.sh
+++ bash/etherpad.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# run Manuskript
+# run Etherpad
 function etherpad() {
 	# development mode is slower than production but DirtyDB is not reccommended for production
 	export NODE_ENV=production
 	${HOME}/Repos/Tools/etherpad-lite/src/bin/run.sh
 }
 
-# update Manuskript
+# update Etherpad
 function etherpad-update() {
 	DIR="$PWD"
 	cd "${HOME}/Repos/Tools/etherpad-lite"
