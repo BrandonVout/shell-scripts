b01218e9cdde721c2f168c600e12ff13c985e0e0
Brandon Vout <git@brandonvout.com>
2024-09-14 00:31:33 -0400

    corrected readonly permission
    Signed-off-by: Brandon Vout <git@brandonvout.com>

--- bash/read_write.sh
+++ bash/read_write.sh
@@ -5,5 +5,6 @@ alias setwrite="chmod +r"       # give read permission to other users
 alias setread="chmod +w"        # give write permission to other users
 alias setexec="chmod +x"        # give execution permission to other users
 alias setnoaccess="chmod 700"   # deny other users access
-alias setreadonly="chmod 755"   # give full access to self, read & execute to others
+alias setreadonly="chmod 744"   # give full access to self, read to others
+alias setreadexec="chmod 755"   # give full access to self, read & execute to others
 alias setfullaccess="chmod 777" # everyone can read, write, and execute
