d7a2eec4ffb9465eccfc9b74ad2a6c58ebe8d1a4
Brandon Vout <git@brandonvout.com>
2024-09-14 00:33:40 -0400

    added set read & write
    Signed-off-by: Brandon Vout <git@brandonvout.com>

--- bash/read_write.sh
+++ bash/read_write.sh
@@ -7,4 +7,5 @@ alias setexec="chmod +x"        # give execution permission to other users
 alias setnoaccess="chmod 700"   # deny other users access
 alias setreadonly="chmod 744"   # give full access to self, read to others
 alias setreadexec="chmod 755"   # give full access to self, read & execute to others
+alias setreadwrite="chmod 766"  # give full access to self, read & write to others
 alias setfullaccess="chmod 777" # everyone can read, write, and execute
